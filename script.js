9bc01062c9b0d7eb9543e652aa9908d658292d8f
Richard Farias <105022084+richale1505@users.noreply.github.com>
2024-03-10 18:25:10 -0400

    Update script.js
    Add resize and reset functions.

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 const gridContainer = document.querySelector('#gridContainer');
-const gridSize = prompt('Enter grid size: ', '16px');
+const resizebtn = document.querySelector('#resize');
+const resetbtn = document.querySelector('#reset');
+const defaultGridSize = 16;
 
-for (let i = 0; i < parseInt(gridSize); i++) {
-    const column = document.createElement('div');
-    column.classList.add('column');
+function startGame(size) {   
+    for (let i = 0; i < size; i++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
     
-    for (let i = 0; i < parseInt(gridSize); i++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        column.appendChild(row);
+        for (let i = 0; i < size; i++) {
+            const row = document.createElement('div');
+            row.classList.add('row');
+            column.appendChild(row);
 
-        row.addEventListener('mouseenter', (e) => {
-            e.target.style.background = 'blue';
-        })
-    }
+            row.addEventListener('mouseenter', (e) => {
+                e.target.style.background = 'blue';
+            })
+        }
 
-    gridContainer.appendChild(column);
+        gridContainer.appendChild(column);
+    }    
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
-/*
-    
-*/
+startGame(defaultGridSize);
 
+resizebtn.addEventListener('click', () => {
+    removeAllChildNodes(gridContainer);
+    const gridSize = parseInt(prompt('Enter grid size: ', '16px'));
+    startGame(gridSize);
+})
+
+resetbtn.addEventListener('click', () => {
+    location.reload();
+});
